d2e8f0266dc77992e572b3d3d7da455c31d75d90
marie <borisova.marie1@gmail.com>
2025-11-20 20:05:29 +0100

    Added POST method

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,24 @@
-let products = {}
 
-fetch('/api/products')
+const productsList = document.getElementById('products');
+let products = [{}]
+const product = {
+    name : String,
+    price : Number,
+}
+
+fetch('/api/product')
 .then(res => res.json())
 .then(data => console.log(data))
+
+
+fetch('api/product', {
+    method : 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+         name : 'Shoe',
+         price: 100
+    })
+})
+    .then(res => res.status)
+    .then(status => console.log(status))
+
