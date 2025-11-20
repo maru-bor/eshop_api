af154dfc8461c47e25117ec825fb636df489c694
Sofia <sofia.hennelova@gmail.com>
2025-11-20 19:48:53 +0100

    Create product, Get all products

--- src/main/java/com/example/eshop_api/controller/ProductController.java
+++ src/main/java/com/example/eshop_api/controller/ProductController.java
@@ -1,25 +1,33 @@
 package com.example.eshop_api.controller;
 
 import com.example.eshop_api.model.Product;
+import com.example.eshop_api.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    ProductRepository productRepository;
+
     @GetMapping
-    public ResponseEntity<List<Product>> getProducts() {
-        List<Product> products = new ArrayList<>();
-        Product product = new Product();
-        product.setName("Shoe");
-        product.setPrice(100);
-        products.add(product);
-        return ResponseEntity.ok(products);}
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product newProduct = new Product();
+        newProduct.setName(product.getName());
+        newProduct.setPrice(product.getPrice());
+        productRepository.save(newProduct);
+        return ResponseEntity.ok(newProduct);
+    }
 
 }
