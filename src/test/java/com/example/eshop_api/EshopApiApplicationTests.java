c03ec68bf91687373961ae29fc5653bdeedbea7c
marie <borisova.marie1@gmail.com>
2025-11-20 19:16:16 +0100

    Initial commit

---
+++ src/test/java/com/example/eshop_api/EshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
