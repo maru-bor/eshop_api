8d00901ab38bd957623cc5a02e80123b9ae6cda0
borisova <borisova.marie1@gmail.com>
2025-12-04 15:30:42 +0100

    Edited ProductCard.tsx component

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,3 +1,19 @@
-export default function ProductCard() {
 
+export default function ProductCard() {
+     return(
+            <>
+                <div id="product-card">
+                <h2>Product Name</h2>
+                   <div id="product-info" >
+                      <p id="desc"> Product Description</p>
+                      <p id="price"> Price: 100 </p>
+
+                      <button onClick={addToCart}>Add test product</button>
+                      <button onClick={removeFromCart}>Remove test product</button>
+                   </div>
+                </div>
+
+
+            </>
+        );
 }
