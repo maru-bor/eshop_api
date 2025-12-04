7a446065661573f1ab69f78c904174d98052249b
borisova <borisova.marie1@gmail.com>
2025-12-04 14:50:30 +0100

    Edited ProductsPage.tsx

--- frontend/src/pages/ProductsPage.tsx
+++ frontend/src/pages/ProductsPage.tsx
@@ -1,12 +1,13 @@
 import type { CartItem } from "../types";
 
 export default function ProductsPage({setShoppingCart,}: { setShoppingCart: React.Dispatch<React.SetStateAction<CartItem[]>>; }) {
-    function addToCart() {
-        setShoppingCart((prev) => [
-            ...prev,
-            { id: 1, name: "Boty" },
-        ]);
+    function addToCart(item: CartItem) {
+           setShoppingCart(prev => [...prev, item]);
+       }
     }
+    function removeFromCart(id: number) {
+               setShoppingCart(prev => prev.filter(i => i.id !== id));
+           }
 
     return (
         <div>
